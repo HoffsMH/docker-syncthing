54d2122caf5d224f07ac172356075f31ded40dd4
HoffsMH <matthecker@pm.me>
2019-07-04 16:53:26 -0500

    lets keep trying

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ docker rm -f syncthing
 docker run -d --restart=always \
   -v /srv/sync:/mnt/tsp/pi_1/data/personal \
   -v /srv/syncthing:/srv/config/syncthing \
-  -p 22000:22000  -p 21027:21027/udp -p 8384:8384 \
+  -p 22000:22000  -p 21027:21027/udp -p 8080:8080 \
   --name syncthing \
   syncthing

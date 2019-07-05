023d92a7170845d46e1b148c4190aef0c44edbbf
HoffsMH <matthecker@pm.me>
2019-07-04 20:00:06 -0500

    go

--- setup_fstab.sh
+++ setup_fstab.sh
@@ -3,7 +3,8 @@ FS="ext4"
 sudo mkdir -p /mnt/sda1/syncthing/data
 sudo mkdir -p /mnt/sda1/syncthing/config
 
-sudo sed -ie "$a\/dev\/sda1 \/mnt\/sda1 $FS defaults,user,rw,noatime 0 0" /etc/fstab
+sudo sed -ie "$a\/dev\/sda1" /etc/fstab
+
 
 sudo mount /dev/sda1/ /mnt/sda1
 

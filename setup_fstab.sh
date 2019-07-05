114b4467de06da542e6f32beb896c0eabeab575d
HoffsMH <matthecker@pm.me>
2019-07-04 20:01:55 -0500

    patience being tested

--- setup_fstab.sh
+++ setup_fstab.sh
@@ -3,7 +3,7 @@ FS="ext4"
 sudo mkdir -p /mnt/sda1/syncthing/data
 sudo mkdir -p /mnt/sda1/syncthing/config
 
-sudo sed -ie "$a\/dev" /etc/fstab
+sudo sed -ie "$adev" /etc/fstab
 
 
 sudo mount /dev/sda1/ /mnt/sda1

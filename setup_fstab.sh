86f384075d8693e0ab6a9dc1ed956284c7b35cc7
HoffsMH <matthecker@pm.me>
2019-07-04 20:09:07 -0500

    well how about this!

--- setup_fstab.sh
+++ setup_fstab.sh
@@ -1,12 +1,14 @@
 FS="ext4"
 
+set -e
+
+sudo sed -ie "\$a/dev/sda1 /mnt/sda1 $FS defaults,noatim,user,rw 0 0" /etc/fstab
+
+sudo mount /dev/sda1/ /mnt/sda1
+
 sudo mkdir -p /mnt/sda1/syncthing/data
 sudo mkdir -p /mnt/sda1/syncthing/config
 
-sudo sed -ie "$adev" /etc/fstab
-
-
-sudo mount /dev/sda1/ /mnt/sda1
 
 sudo chmod 777 /mnt/sda1/syncthing/data
 sudo chmod 777 /mnt/sda1/syncthing/config
